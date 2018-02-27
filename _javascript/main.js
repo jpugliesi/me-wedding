164f4960ac382c04b29e249c9eccc40aff257e72
John Pugliesi <jpugliesi12@gmail.com>
2018-02-26 19:13:20 -0800

    Initial commit

---
+++ _javascript/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Bulma!');
+});
+
+document.body.classList.add('js-loading');
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.body.classList.remove('js-loading');
+}
